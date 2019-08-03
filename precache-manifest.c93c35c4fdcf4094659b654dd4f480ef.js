54e3e30498cb88dc55381392ffb825defba208d2
kdilot <kdilot@gmail.com>
2019-08-03 14:25:55 -0700

    Updates

---
+++ precache-manifest.c93c35c4fdcf4094659b654dd4f480ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e0f62d131fe85276a297c88aa163fe",
+    "url": "/buttons/index.html"
+  },
+  {
+    "revision": "92927a24cee2fcfecde0",
+    "url": "/buttons/static/css/main.cf45c1b6.chunk.css"
+  },
+  {
+    "revision": "0ddfb1f2871dae6f354d",
+    "url": "/buttons/static/js/2.d8a1cfcd.chunk.js"
+  },
+  {
+    "revision": "92927a24cee2fcfecde0",
+    "url": "/buttons/static/js/main.a70ba891.chunk.js"
+  },
+  {
+    "revision": "f5de2200a6b24808c658",
+    "url": "/buttons/static/js/runtime~main.72d94556.js"
+  }
+]);
